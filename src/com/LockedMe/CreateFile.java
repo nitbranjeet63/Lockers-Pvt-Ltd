68ffc42f935dddfa7a232d63a8c66df2c4f147a2
OmSai <OmSai@DESKTOP-LPRIUFA>
2021-07-21 16:04:26 +0530

    Create File class

--- src/com/LockedMe/CreateFile.java
+++ src/com/LockedMe/CreateFile.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class CreateFile {
 	public void createfile() {
 		@SuppressWarnings("resource")
-		Scanner sc  = new Scanner(System.in);
+		Scanner sc  = new Scanner(System.in);//Creating Object of Scanner Class
 		try {
 			System.out.println("Enter the file name: ");
-			String filename=sc.nextLine()+".txt";
-			FileOutputStream fileout = new FileOutputStream(filename,true);
+			String filename=sc.nextLine()+".txt";//Saving file with .txt Extension for readability
+			FileOutputStream fileout = new FileOutputStream(filename,true);//Creating Object of FileOutputStream Class and file will be create to the current directory
 			System.out.println("Enter file content:");
 			String str =sc.nextLine()+"\n";
-			byte[] b = str.getBytes();
-			fileout.write(b);
+			byte[] b = str.getBytes();//Converting String to Array
+			fileout.write(b);//Performing write operation on file
 			fileout.close();
 			System.out.println("File saved");
 		
